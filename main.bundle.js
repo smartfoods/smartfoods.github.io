f9296352c50bd8a99f13dadadab33eed1f33f134
Daniel Aguiar <bsb.aguiar@gmail.com>
2018-02-27 16:19:02 -0300

    build

--- main.bundle.js
+++ main.bundle.js
@@ -2666,10 +2666,14 @@ var _a;
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `angular-cli.json`.
 
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.environment = {
-    production: true
+    production: false
 };
 //# sourceMappingURL=E:/paladar-fit/frontend-angular/src/environment.js.map
 
